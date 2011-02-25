382538b20a2816e1e8177d4e6b8018431e4a9ae3
Alexis Gallagher <alexis@alexisgallagher.com>
2011-02-25 12:04:26 +0000

    initial commit

---
+++ clustersearch.py
@@ -0,0 +1,65 @@
+
+alphabet = "CGAU"
+def mut(point):
+    """Returns set of all neighbors of POINT"""
+    neighbors = list()
+    for pos in xrange(len(point)): # for every position  ...
+        ch = point[pos]            # ... find its alternative values
+        others = alphabet.replace(ch,'')
+        for newch in others:       # ... and build resulting mutants
+            variant = newch.join((point[:pos],point[pos+1:]))
+            neighbors.append(variant)
+    return neighbors
+
+colors = ["white", "black", "red"]
+def colorOf(point):
+    """Given a point, computes its color"""
+    import random
+    return random.choice(colors)
+
+def search(root):
+    """Searches breadth-first from root node, recording color of every
+    node observed, but traversing onward only from "white" nodes.
+
+    Nodes x and y are connected if (x in mut(y)). Each node x has
+    color colorOf(x).
+
+    Returns a dictionary of the observed nodes and their colors."""
+    SPECIAL_COLOR = "white"
+    
+    observed_nodes = dict()
+    observed_nodes[root]=SPECIAL_COLOR
+    
+    traversed = list()
+
+    to_traverse = list()
+    to_traverse.append(root)
+
+    while to_traverse:
+        # process head of queue of nodes still to be traversed
+        cursor = to_traverse.pop(0)
+
+        print("Traversing from node %s" % cursor)
+
+        # compute the neighbors not previously observed
+        neighbors = set(mut(cursor)) - set(observed_nodes.keys())
+
+        print("\tFound %s had neighbors %s of which the previously unobserved were %s" % (cursor, mut(cursor), neighbors))
+
+        if neighbors:
+            # else, assign them colors
+            newnodes = dict([(s,colorOf(s)) for s in neighbors])
+            print("\tObserved these nodes to be colored: %s" % newnodes)
+            # plan to visit only the special ones later
+            to_traverse.extend([i for i in neighbors if newnodes[i]==SPECIAL_COLOR])
+            # add them to the db of observed nodes
+            observed_nodes.update(newnodes)
+            # mark the current node as traversed
+            traversed.append(cursor)
+
+    return (observed_nodes,traversed)
+
+# def dorun(length,alphabetsize,colors):
+#     alphabet = "FGHI"
+#     return search("FF")
+    
