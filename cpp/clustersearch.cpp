9189bb9605b454f00ceb415fba4e4645fe1af2df
Alexis Gallagher <alexis@alexisgallagher.com>
2011-03-03 19:28:17 +0000

    edits

--- cpp/clustersearch.cpp
+++ cpp/clustersearch.cpp
@@ -17,34 +17,34 @@ using std::set;
 typedef string geno;
 typedef string pheno;
 
-// key iterators on maps
+pheno colors[] = {"white", "red", "blue"};
+/**
+FIXME: implement
+*/
+set<geno> mut(geno g) { return set<geno>; }
 
+pheno colorOf(geno g) { return colors[0]; }
 
+// iterators over map keys
 typedef map<geno,pheno>::iterator map_iterator;
-typedef map<geno,pheno>::value_type
+typedef map<geno,pheno>::key_type (*get_key_t)(map<geno,pheno>::value_type);
+typedef boost::transform_iterator<get_key_t, map_iterator> key_iterator;
+map<Key,Val>::key_type get_key(map<Key,Val>::value_type aPair) { return aPair.first; }
 
-typedef boost::function
-typedef map::<geno,pheno>::key_type (*F)(map<geno,pheno>::value_type);
-typedef boost::transform_iterator<F,map_iterator> mapkey_iterator;
+/**
+   Searches breadth-first from root node, recording color of every
+   node observed, but traversing onward only from nodes with the color
 
+   Nodes x and y are connected if (x in mut(y)). Each node x has
+   color colorOf(x).
 
-pheno range[] = {"white", "red", "blue"};
-
-set<geno> mut(geno g) {
-  return set<geno>;
-}
-
-set<geno> keys(map<geno> m) {
-  set<geno> result;
-  
-}
-
-
+   Returns a std::map of the observed nodes and their colors.
+*/
 map<geno,pheno> search(const geno& root) {
-  const pheno SPECIAL_COLOR = range[0];
+  const pheno SPECIAL_COLOR = colors[0];
 
   map<geno,pheno> observed;
-  observed[geno]=SPECIAL_COLOR;
+  observed[root]=SPECIAL_COLOR;
 
   list<geno> to_traverse;
   to_traverse.push_back(root);
@@ -54,18 +54,22 @@ map<geno,pheno> search(const geno& root) {
     cursor = to_traverse.front();
     to_traverse.pop_front();
     set<geno> all_neighbors(mut(cursor));
-    set<geno> all_observed(keys(observed));
-    set<geno> new_neighbors;
-    set_difference(all_neighbors.begin(),all_neighbors.end()
-		   all_observed.begin(),all_observed.end(),
+    list<geno> new_neighbors;
+    set_difference(all_neighbors.begin(),all_neighbors.end(),
+		   key_iterator(observed.begin(), get_key), key_iterator(observed.end(), get_key),
 		   std::inserter(new_neighbors, new_neighbors.end()));
 
     if(!new_neighbors.empty()) {
-      
-    
-
-
+      set<geno> newnodes;
+      for(list<geno>::iterator g = new_neighbors.begin(); g != new_neighbors.end(); ++g) {
+	newnodes[*g] = colorOf(*g);
+	if (newnodes[*g] == SPECIAL_COLOR) 
+	  to_traverse.push_back(*g);
+      }
+      observed.insert(newnodes.begin(),newnodes.end());
+    }
   }
-
+  return observed;
 }
+      
 
