fc67c8d75c5d18e23b471560acba9d2e9cfc4b60
Alexis Gallagher <alexis@alexisgallagher.com>
2011-03-10 09:32:15 +0000

    split printing functions into own header

--- cpp/clustersearch.cpp
+++ cpp/clustersearch.cpp
@@ -49,71 +49,6 @@ string createRootString(size_t length) {
 }
 
 
-/* prints a list<T> */
-template <class T>
-std::ostream & operator<<(std::ostream & out, list<T> & s) {
-  out << "[";
-  for(typename list<T>::iterator item_it = s.begin(); item_it != s.end(); ++item_it) {
-    out << *item_it << " ";
-  }
-  out << "]";
-  return out;
-}      
-
-/* prints a unordered_set<T> */
-template <class T>
-std::ostream & operator<<(std::ostream & out, unordered_set<T> & s) {
-  out << "{";
-  for(typename unordered_set<T>::iterator item_it = s.begin(); item_it != s.end(); ++item_it) {
-    out << *item_it << " ";
-  }
-  out << "}";
-  return out;
-}      
-
-/* prints a vector<T> */
-template <class T>
-std::ostream & operator<<(std::ostream & out, vector<T> & s) {
-  out << "{";
-  for(typename vector<T>::iterator item_it = s.begin(); item_it != s.end(); ++item_it) {
-    out << *item_it << " ";
-  }
-  out << "}";
-  return out;
-}      
-/* prints a set<T> */
-template <class T>
-std::ostream & operator<<(std::ostream & out, set<T> & s) {
-  out << "{";
-  for(typename set<T>::iterator item_it = s.begin(); item_it != s.end(); ++item_it) {
-    out << *item_it << " ";
-  }
-  out << "}";
-  return out;
-}      
-
-/* prints a unordered_map<TKey,TVal> */
-template <class TKey, class TVal>
-std::ostream & operator<<(std::ostream & out, unordered_map<TKey,TVal> & m) {
-  out << "{";
-  for(typename unordered_map<TKey,TVal>::iterator item_it = m.begin(); item_it != m.end(); ++item_it) {
-    out << item_it->first << ": " << item_it->second << ", ";
-  }
-  out << "}";
-  return out;
-}      
-
-/* prints a map<TKey,TVal> */
-template <class TKey, class TVal>
-std::ostream & operator<<(std::ostream & out, std::map<TKey,TVal> & m) {
-  out << "{";
-  for(typename std::map<TKey,TVal>::iterator item_it = m.begin(); item_it != m.end(); ++item_it) {
-    out << item_it->first << ": " << item_it->second << ", ";
-  }
-  out << "}";
-  return out;
-}      
-
 /**
    Returns mutants
 */
@@ -228,7 +163,7 @@ int main()
 
   if (true) {
     // benchmark 10 random searches
-    for(int i =0; i < 10; ++i) {
+    for(int i =0; i < 20; ++i) {
       search(g);
     }
   }
---
+++ cpp/printable.hpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <set>
+#include <list>
+#include <map>
+#include <vector>
+
+
+/* prints a list<T> */
+template <class T>
+std::ostream & operator<<(std::ostream & out, std::list<T> & s) {
+  out << "[";
+  for(typename std::list<T>::iterator item_it = s.begin(); item_it != s.end(); ++item_it) {
+    out << *item_it << " ";
+  }
+  out << "]";
+  return out;
+}      
+
+/* prints a unordered_set<T> */
+template <class T>
+std::ostream & operator<<(std::ostream & out, std::tr1::unordered_set<T> & s) {
+  out << "{";
+  for(typename std::tr1::unordered_set<T>::iterator item_it = s.begin(); item_it != s.end(); ++item_it) {
+    out << *item_it << " ";
+  }
+  out << "}";
+  return out;
+}      
+
+/* prints a vector<T> */
+template <class T>
+std::ostream & operator<<(std::ostream & out, std::vector<T> & s) {
+  out << "{";
+  for(typename std::vector<T>::iterator item_it = s.begin(); item_it != s.end(); ++item_it) {
+    out << *item_it << " ";
+  }
+  out << "}";
+  return out;
+}      
+/* prints a set<T> */
+template <class T>
+std::ostream & operator<<(std::ostream & out, std::set<T> & s) {
+  out << "{";
+  for(typename std::set<T>::iterator item_it = s.begin(); item_it != s.end(); ++item_it) {
+    out << *item_it << " ";
+  }
+  out << "}";
+  return out;
+}      
+
+/* prints a unordered_map<TKey,TVal> */
+template <class TKey, class TVal>
+std::ostream & operator<<(std::ostream & out, std::tr1::unordered_map<TKey,TVal> & m) {
+  out << "{";
+  for(typename std::tr1::unordered_map<TKey,TVal>::iterator item_it = m.begin(); 
+      item_it != m.end(); ++item_it) {
+    out << item_it->first << ": " << item_it->second << ", ";
+  }
+  out << "}";
+  return out;
+}      
+
+/* prints a map<TKey,TVal> */
+template <class TKey, class TVal>
+std::ostream & operator<<(std::ostream & out, std::map<TKey,TVal> & m) {
+  out << "{";
+  for(typename std::map<TKey,TVal>::iterator item_it = m.begin(); item_it != m.end(); ++item_it) {
+    out << item_it->first << ": " << item_it->second << ", ";
+  }
+  out << "}";
+  return out;
+}      
+
