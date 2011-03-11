211291ced6777cbad2eb7ac7ab19ea1df6d376fa
Alexis Gallagher <alexis@alexisgallagher.com>
2011-03-11 16:51:02 +0000

    commented public interfaces

--- cpp/clustersearchwrap.py
+++ cpp/clustersearchwrap.py
@@ -1,3 +1,12 @@
+"""
+This module wraps the dynamic library clustersearch.dylib.
+
+It defines three functions for external use:
+  calculate_measures_as_tuple
+  calculate_measures
+  srand
+"""
+
 from ctypes.util import find_library
 from ctypes import cdll
 from ctypes import c_uint
@@ -5,12 +14,12 @@ from ctypes import c_uint
 libclustersearch = cdll.LoadLibrary(find_library('clustersearch'))
 
 # load function srand
-srand = libclustersearch.srand
-srand.argtypes = [ c_uint ]
+_srand = libclustersearch.srand
+_srand.argtypes = [ c_uint ]
 
 # load function calculate_measures
-calculate_measures = libclustersearch.calculate_measures
-calculate_measures.argtypes = [ c_uint,c_uint,c_uint]
+_calculate_measures = libclustersearch.calculate_measures
+_calculate_measures.argtypes = [ c_uint,c_uint,c_uint]
 
 #from ctypes import *
 from ctypes import Structure
@@ -27,9 +36,31 @@ exits_size     = %s""" % (self.cluster_size ,self.perimeter_size ,self.colors ,s
         return s
     pass
 
-calculate_measures.restype = cluster_measure
+_calculate_measures.restype = cluster_measure
 
 def calculate_measures_as_tuple(length,alphabetsize,colors):
-    x = calculate_measures(length,alphabetsize,colors)
+    "Returns tuple of basic measures from a single cluster search"
+    x = _calculate_measures(length,alphabetsize,colors)
     return (x.cluster_size ,x.perimeter_size ,x.colors ,x.exits_size)
 
+def srand(seed):
+    """Seeds the random number generator.
+
+    Reseeding to the same start value (e.g., 0), should cause
+    subsequent searches to produce the same result. But it does not. I
+    don't know why.
+    """
+    _srand(seed)
+
+def calculate_measures(length,alphabetsize,colors):
+    "Returns dict of basic measures from a single cluster search"
+    t = calculate_measures_as_tuple(length,alphabetsize,colors)
+    d = dict()
+    d["cluster_size"] = t[0]
+    d["perimeter_size"] = t[1]
+    d["colors"] = t[2]
+    d["exits_size"] = t[3]
+    return d
+
+
+    
