6493cbe15e46c5abecf5d27f1b8ca8f675dff3c1
Alexis Gallagher <alexis@alexisgallagher.com>
2011-03-11 15:23:57 +0000

    hacking. not working. not tested.

--- cpp/clustersearchwrap.py
+++ cpp/clustersearchwrap.py
@@ -12,6 +12,18 @@ calculate_measures = libclustersearch.calculate_measures
 calculate_measures.argtypes = [ c_uint,c_uint,c_uint]
 
 
+#from ctypes import *
+from ctypes import Structure
+class cluster_measure(Structure):
+    _fields_ = [("cluster_size", c_uint),
+                ("perimeter_size", c_uint),
+                ("colors", c_uint),
+                ("exits_size", c_uint)]
+    pass
+
+calculate_measures.restype = cluster_measure
+
+
 
 
 
