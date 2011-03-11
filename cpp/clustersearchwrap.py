ba1a27a2e3a6ab84e790c6f7d5e04a94ad1f23a6
Alexis Gallagher <alexis@alexisgallagher.com>
2011-03-11 16:10:24 +0000

    edits

--- cpp/clustersearchwrap.py
+++ cpp/clustersearchwrap.py
@@ -21,11 +21,24 @@ class cluster_measure(Structure):
                 ("perimeter_size", c_uint),
                 ("colors", c_uint),
                 ("exits_size", c_uint)]
+    def __repr__(self):
+        s= """cluster_size   = %s
+perimeter_size = %s
+colors         = %s
+exits_size     = %s""" %        (self.cluster_size   ,self.perimeter_size ,self.colors         ,self.exits_size     )
+        return s
     pass
+
+
 
 calculate_measures.restype = cluster_measure
 
-
+def display(x):
+    print x.cluster_size
+    print x.perimeter_size
+    print x.colors
+    print x.exits_size
+    
 
 
 
