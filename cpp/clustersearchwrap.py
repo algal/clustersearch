e27a45ca190c33957fff110df50c503b1510d190
Alexis Gallagher <alexis@alexisgallagher.com>
2011-03-11 15:12:42 +0000

    bugfix re ctype for inputs

--- cpp/clustersearchwrap.py
+++ cpp/clustersearchwrap.py
@@ -1,10 +1,18 @@
 from ctypes.util import find_library
 from ctypes import cdll
-from ctypes import c_unit
+from ctypes import c_uint
 
 libclustersearch = cdll.LoadLibrary(find_library('clustersearch'))
 
 # define wrapper for C func cluster_size
 foo = libclustersearch.cluster_size
 foo.argtypes = [ c_uint,c_uint,c_uint]
+
+calculate_measures = libclustersearch.calculate_measures
+calculate_measures.argtypes = [ c_uint,c_uint,c_uint]
+
+
+
+
+
 
