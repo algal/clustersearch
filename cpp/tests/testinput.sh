ccddca6aa0b3526fa59ebaa53635ebb0e8560ca9
Alexis Gallagher <alexis@alexisgallagher.com>
2011-04-08 16:24:32 +0100

    add testcase for pdfstring

--- cpp/tests/testinput.sh
+++ cpp/tests/testinput.sh
@@ -9,4 +9,6 @@
 ../build/clusters --verbose=0 --alpha=3 --length=8 --colors=3 --seed=6 --samples=5
 ../build/clusters --verbose=0 --alpha=3 --length=8 --colors=3 --seed=6 --samples=5 --mode=data
 ../build/clusters --verbose=0 --alpha=3 --length=8 --colors=3 --seed=6 --samples=50 --mode=data --gray=0.3
+../build/clusters --verbose=0 --alpha=3 --length=8 --colors=4 --seed=6 --samples=50 --mode=data --pdf=0.3,0.2,0.25,0.25
+
 
